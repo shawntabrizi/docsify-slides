22244ef857c8109d3fb5b71c7bd722674ee2be9f
Shawn Tabrizi <shawntabrizi@gmail.com>
2025-11-14 00:52:44 -0400

    update deps

--- rollup.config.base.js
+++ rollup.config.base.js
@@ -1,7 +1,7 @@
-import commonjs from 'rollup-plugin-commonjs'
-import nodejs from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
 import postcss from 'rollup-plugin-postcss'
+import babel from '@rollup/plugin-babel';
+import nodejs from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.js',
